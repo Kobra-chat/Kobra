11fb65b89b5b1a7a7f5fcd8b3069542092d82bbc
Kobra <vencel.vencel.vencel@gmail.com>
2026-07-23 12:18:36 +0200

    Update firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAytpBEZNb4cEsbpsuWz2zpszkxCTDCN2g",
+  authDomain: "kobra-d3464.firebaseapp.com",
+  databaseURL: "https://kobra-d3464-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "kobra-d3464",
+  storageBucket: "kobra-d3464.firebasestorage.app",
+  messagingSenderId: "127800460763",
+  appId: "1:127800460763:web:5e690d2638a4b7aaf093fe"
+};
